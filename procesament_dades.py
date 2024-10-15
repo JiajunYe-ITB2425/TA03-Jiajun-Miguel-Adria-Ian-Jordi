3afae76e92c2afc3836dd773872262d399a46244
JiajunYe-ITB2425 <jiajun.ye.7e6@itb.cat>
2024-10-15 10:16:59 +0200

    Add files via upload

---
+++ procesament_dades.py
@@ -0,0 +1,7 @@
+'''
+Grup 3: Jiajun, Ian, Miguel, Adria, Jordi
+Data: 25 d'octubre de 2024
+Professor: Javier Amaya
+Asignatura: ASIXcB MDS TA03
+Descripció: llegueix les dades XML obtingudes per un fitxer i mostria per pantalla la informació obtinguda.
+'''
