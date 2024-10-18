668c67f7e24f368d726e8b311ed5bd0a4a940615
Jiajun <jiajun.ye.7e6@itb.cat>
2024-10-18 12:55:56 +0200

    1

--- procesament_dades2_dates.py
+++ procesament_dades2_dates.py
@@ -18,19 +18,22 @@ lista = []
 tree = ET.parse(archivo_xml)
 root = tree.getroot()
 
+def validar_fecha(fecha_incidencia):
+    if int(fecha_incidencia[6:10]) <= datetime.now().year and int(fecha_incidencia[3:5]) <= datetime.now().month and int(fecha_incidencia[3:5]) != "07" and int(fecha_incidencia[3:5]) != "08":
+        return True
+    else:
+        return False
+
 # Bucle para hacer la lista
 for fila in root.findall('row'):
     fecha_incidencia = fila.find('DATA_DE_LA_INCIDÈNCIA').text
-    fecha_incidencia_dt = datetime.strptime(fecha_incidencia, '%d/%m/%Y')
-
-    # Verifica si la fecha es válida
-    if fecha_incidencia_dt > datetime.now():
+    if validar_fecha(fecha_incidencia):
         fila_info = [
             fila.find('Marca_de_temps').text,
             fila.find('Adreça_electrònica').text,
             fila.find('Informació_relativa_sobre_la_protecció_de_dades').text,
             fila.find('NOM_i_COGNOMS_Formador_a_que_obre_incidència').text,
-            fecha_incidencia,
+            fila.find('DATA_DE_LA_INCIDÈNCIA').text,
             fila.find('TIPUS_INCIDÈNCIA').text,
             fila.find('AULA_SALAConsulteu_cartell_d_entrada_a_l_aula_o_l_espai_També_disposeu_d_aquesta_informació_a_trainers_itb_cat_espais').text,
             fila.find('EQUIPS_i_o_SERVEIS_AFECTATSNúmero_SACE_seregrafiat_si_disposa_').text,
@@ -38,7 +41,6 @@ for fila in root.findall('row'):
             fila.find('DESCRIPCIÓ_PROPOSTA_DE_SOLUCIOLa_vostra_resposta').text,
             fila.find('NIVELL_URGENCIA_DE_SOLUCIÓ').text
         ]
-        # Añadie los datos de la fila a la lista
+        # Añade los datos de la fila a la lista
         lista.append(fila_info)
-
 print(lista)
